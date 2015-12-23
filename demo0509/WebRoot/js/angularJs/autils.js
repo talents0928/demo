843e314f315ffe72b1925551bdd093b9ba318279
he <1191644066@qq.com>
2015-12-23 19:16:48 +0800

    commit

---
+++ demo0509/WebRoot/js/angularJs/autils.js
@@ -0,0 +1,14 @@
+
+
+
+
+angular.module('Autils',[])
+.controller('uCtrl',['$scope',function($scope){
+	var student = {};
+	$scope.student = student ;
+	student.name = '何川';
+	student.age = '45' ;
+	
+}])
+
+;
